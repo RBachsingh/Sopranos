cc975854ad0b363b187610f98016a955df746471
RBachsingh <71096988+RBachsingh@users.noreply.github.com>
2023-03-22 11:16:19 +0100

    ye

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,17 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(ManagerRegistry $doctrine): Response
-    {
-        $cat = $doctrine->getRepository(Category::class)->findAll();
-
-        $categoryName = $cat;
-
-        return $this->render('home/index.html.twig', [
-            'category' => $categoryName,
-        ]);
-    }
+//    #[Route('/', name: 'home')]
+//    public function index(ManagerRegistry $doctrine): Response
+//    {
+//        $cat = $doctrine->getRepository(Category::class)->findAll();
+//
+//        $categoryName = $cat;
+//
+//        return $this->render('home/index.html.twig', [
+//            'category' => $categoryName,
+//        ]);
+//    }
     #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Category;
 use App\Entity\Products;
 use Doctrine\Persistence\ManagerRegistry;
 
